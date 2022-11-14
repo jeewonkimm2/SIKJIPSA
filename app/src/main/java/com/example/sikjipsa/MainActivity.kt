585e751f9160af68932aafdcff53b6fcda9b7f78
moon34git <moon34git@gmail.com>
2022-11-14 18:14:14 +0900

    ㅈ훈

--- app/src/main/java/com/example/sikjipsa/MainActivity.kt
+++ app/src/main/java/com/example/sikjipsa/MainActivity.kt
@@ -15,6 +15,7 @@ import com.google.android.material.navigation.NavigationView
 import java.util.jar.Manifest
 //수오가 침
 //지원
+//후니
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
