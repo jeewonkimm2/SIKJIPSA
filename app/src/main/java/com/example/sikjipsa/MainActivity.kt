1bdc5ae74576936add3ca8211b40ae712168cbba
moon34git <moon34git@gmail.com>
2022-11-15 21:06:57 +0900

    Insert the weather API into the fragment_alarm.xml

--- app/src/main/java/com/example/sikjipsa/MainActivity.kt
+++ app/src/main/java/com/example/sikjipsa/MainActivity.kt
@@ -7,12 +7,14 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -23,6 +25,9 @@ import com.example.sikjipsa.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.navigation.NavigationView
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URL
 import java.util.*
 import java.util.jar.Manifest
 //수오가 침
@@ -30,7 +35,8 @@ import java.util.jar.Manifest
 //후니
 class MainActivity : AppCompatActivity(){
 
-
+    val CITY: String = "seoul,kr"
+    val API: String = "a5b5e79e80276333510e3446c4d3498b"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +48,6 @@ class MainActivity : AppCompatActivity(){
         //BottomNavigation
         initView()
         initBottomNavigation()
-
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
@@ -72,6 +77,7 @@ class MainActivity : AppCompatActivity(){
                 R.id.action_favorite_alarm -> {
                     var alarmFragment = AlarmFragment()
                     supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                    weatherTask().execute()
                     return@setOnItemSelectedListener true
                 }
                 R.id.action_account -> {
@@ -89,7 +95,62 @@ class MainActivity : AppCompatActivity(){
 
     lateinit var binding: ActivityMainBinding
 
+    inner class weatherTask(): AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
 
+        }
+
+        override fun doInBackground(vararg p0: String?): String? {
+            var response: String?
+            try {
+                response =
+                    URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API")
+                        .readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                response = null
+            }
+            return response
+        }
+
+
+        override fun onPostExecute(result: String?) {
+
+            super.onPostExecute(result)
+            try{
+                val jsonObj = JSONObject(result)
+                val main  = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updateAtText = "Updated at: "+ java.text.SimpleDateFormat(
+                    "dd/mm/yyyy hh:mm a",
+                    Locale.ENGLISH
+                ).format(
+                    Date(updatedAt * 1000)
+                )
+                val temp = main.getString("temp")+"C"
+                val tempMin = "Min Temp: "+main.getString("temp_min")+"C"
+                val tempMax = "Max Temp: "+main.getString("temp_max")+"C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+
+
+
+                findViewById<TextView>(R.id.locationTxt).text = address
+                findViewById<TextView>(R.id.temperatureTxt).text = temp
+            }catch (e: Exception){
+
+            }
+        }
+    }
 
 
 
--- app/src/main/java/com/example/sikjipsa/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/sikjipsa/navigation/AlarmFragment.kt
@@ -1,19 +1,32 @@
 package com.example.sikjipsa.navigation
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.ViewUtils
 import androidx.fragment.app.Fragment
 import com.example.sikjipsa.R
+import com.example.sikjipsa.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AlarmFragment : Fragment(){
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
+
         return view
     }
+
+
 }
