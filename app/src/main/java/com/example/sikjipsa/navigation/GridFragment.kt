39ebf785c40430b07f0e0d272601d730cd2cf8e5
jeewonkimm2 <jeewoncoding@gmail.com>
2022-11-15 09:04:57 +0900

    검색 기능 수정 필

--- app/src/main/java/com/example/sikjipsa/navigation/GridFragment.kt
+++ app/src/main/java/com/example/sikjipsa/navigation/GridFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sikjipsa.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.sikjipsa.R
 import com.example.sikjipsa.databinding.FragmentGridBinding
+import kotlinx.android.synthetic.main.fragment_grid.*
 
 class GridFragment : Fragment(){
 
@@ -15,13 +17,18 @@ class GridFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
-        return view
-
-
-        
+//        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
+        return inflater.inflate(R.layout.fragment_grid, container, false)
 
     }
+
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        image_load_btn.setOnClickListener{
+//            var intent = Intent(getActivity(), SearchActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 
 
 
