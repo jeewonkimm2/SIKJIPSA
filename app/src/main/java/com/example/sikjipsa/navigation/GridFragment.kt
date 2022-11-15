a08bd3f0f6be13f804296ea1445d9f605a98be47
jeewonkimm2 <jeewoncoding@gmail.com>
2022-11-15 10:00:39 +0900

    프레그먼트->액티비티 이동 수정 필

--- app/src/main/java/com/example/sikjipsa/navigation/GridFragment.kt
+++ app/src/main/java/com/example/sikjipsa/navigation/GridFragment.kt
@@ -2,13 +2,13 @@ package com.example.sikjipsa.navigation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.sikjipsa.R
-import com.example.sikjipsa.databinding.FragmentGridBinding
-import kotlinx.android.synthetic.main.fragment_grid.*
+import kotlinx.android.synthetic.main.fragment_grid.image_load_btn
 
 class GridFragment : Fragment(){
 
@@ -17,19 +17,25 @@ class GridFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
-        return inflater.inflate(R.layout.fragment_grid, container, false)
 
+//        왜안될까?
+        fun onActivityCreated(savedInstanceState: Bundle?) {
+            super.onActivityCreated(savedInstanceState)
+            image_load_btn.setOnClickListener{
+                activity?.let{
+                    Log.d("ITM","클릭")
+                    val intent = Intent(getActivity(), SearchActivity::class.java)
+                    Log.d("ITM","클릭1")
+                    startActivity(intent)
+                }
+            }
+        }
+
+        return inflater.inflate(R.layout.fragment_grid, container, false)
     }
 
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        image_load_btn.setOnClickListener{
-//            var intent = Intent(getActivity(), SearchActivity::class.java)
-//            startActivity(intent)
-//        }
-//    }
 
 
 
 }
+
