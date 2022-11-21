59a24ed59cbd5640bf51b3164a4666f881bcde46
jeewonkimm2 <jeewoncoding@gmail.com>
2022-11-22 02:07:43 +0900

    Model result extraction ongoing

--- app/src/main/java/com/example/sikjipsa/SearchActivity.kt
+++ app/src/main/java/com/example/sikjipsa/SearchActivity.kt
@@ -24,6 +24,8 @@ import org.tensorflow.lite.Interpreter
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.Byte
+import java.lang.Float
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.util.*
@@ -58,7 +60,7 @@ class SearchActivity : AppCompatActivity() {
             fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
                 super.onActivityResult(requestCode, resultCode, data)
                 if (requestCode == PICK_IMAGE_FROM_ALBUM) {
-                    if (resultCode == Activity.RESULT_OK) {
+                    if (resultCode == RESULT_OK) {
                         photoURI = data?.data
                         addphoto_image.setImageURI(photoURI)
                     } else {
@@ -68,6 +70,13 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
+
+//        모델 입력 출력 결과
+//        1 input(s):
+//        [  1 224 224   3] <class 'numpy.uint8'>
+//
+//        1 output(s):
+//        [ 1 18] <class 'numpy.uint8'>
 
         var interpreter:Interpreter?=null
 
@@ -81,79 +90,73 @@ class SearchActivity : AppCompatActivity() {
             .addOnSuccessListener { model: CustomModel? ->
                 val modelFile = model?.file
                 if (modelFile != null) {
-
                     Log.d("tflite성공","모델 import성공")
-
                     interpreter = Interpreter(modelFile)
                 }
             }
-        val drawable = getResources().getDrawable(R.drawable.sunflower)
+
+
+        val drawable = getResources().getDrawable(R.drawable.monstera)
         val bitmap = Bitmap.createScaledBitmap(drawable.toBitmap(), 224, 224, true)
+        Log.d("typecheck","${bitmap.javaClass.name}")
+//        android.graphics.Bitmap
         val input = ByteBuffer.allocateDirect(224*224*3*4).order(ByteOrder.nativeOrder())
         for (y in 0 until 224) {
             for (x in 0 until 224) {
                 val px = bitmap.getPixel(x, y)
 
                 // Get channel values from the pixel value.
-                val r = Color.red(px)
-                val g = Color.green(px)
-                val b = Color.blue(px)
+                val r:Int = Color.red(px)
+                val g:Int = Color.green(px)
+                val b:Int = Color.blue(px)
 
                 // Normalize channel values to [-1.0, 1.0]. This requirement depends on the model.
                 // For example, some models might require values to be normalized to the range
                 // [0.0, 1.0] instead.
-                val rf = (r - 127) / 255
-                val gf = (g - 127) / 255
-                val bf = (b - 127) / 255
+//                val rf = (r - 127) / 255
+//                val gf = (g - 127) / 255
+//                val bf = (b - 127) / 255
+//
+//                input.putInt(rf)
+//                input.putInt(gf)
+//                input.putInt(bf)
+                input.putInt(r)
+                input.putInt(g)
+                input.putInt(b)
 
-                input.putInt(rf)
-                input.putInt(gf)
-                input.putInt(bf)
             }
         }
 
-        val bufferSize = 1000 * java.lang.Float.SIZE / java.lang.Byte.SIZE
+
+        val bufferSize = 18 * Integer.SIZE / java.lang.Byte.SIZE
         val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
         interpreter?.run(input, modelOutput)
-
-        modelOutput.rewind()
-        val probabilities = modelOutput.asFloatBuffer()
-        Log.d("tflite성공인가","${probabilities.get(0)}")
-        Log.d("tflite성공인가","${probabilities.get(1)}")
-        Log.d("tflite성공인가","${probabilities.get(2)}")
-        Log.d("tflite성공인가","${probabilities.get(3)}")
-        Log.d("tflite성공인가","${probabilities.get(4)}")
-        Log.d("tflite성공인가","${probabilities.get(5)}")
-        Log.d("tflite성공인가","${probabilities.get(6)}")
-        Log.d("tflite성공인가","${probabilities.get(7)}")
-        Log.d("tflite성공인가","${probabilities.get(8)}")
-        Log.d("tflite성공인가","${probabilities.get(9)}")
-        Log.d("tflite성공인가","${probabilities.get(10)}")
-        Log.d("tflite성공인가","${probabilities.get(11)}")
-        Log.d("tflite성공인가","${probabilities.get(12)}")
-        Log.d("tflite성공인가","${probabilities.get(13)}")
-        Log.d("tflite성공인가","${probabilities.get(14)}")
-        Log.d("tflite성공인가","${probabilities.get(15)}")
-        Log.d("tflite성공인가","${probabilities.get(16)}")
-        Log.d("tflite성공인가","${probabilities.get(17)}")
-        Log.d("tflite성공인가","${probabilities.get(18)}")
+        Log.d("inputhh","$input $modelOutput")
+        for(i in 0 until 72){
+            Log.d("inputhh","${modelOutput[i]}")
+        }
 
 
-        try {
-//            라벨 읽기
-//            reader.readLine() : 라벨읽음
-            val reader = BufferedReader(
-                InputStreamReader(assets.open("labels.txt")))
-//            Log.d("tflite성공인가","${reader.readLine()}")
-//            Log.d("tflite성공인가","${probabilities.capacity()}")
-//            for (i in probabilities.capacity()) {
+
+//        modelOutput.rewind()
+////        probabilties.get(19)부터 에러
+//        val probabilities = modelOutput.asIntBuffer()
+//
+//        try {
+////            라벨 읽기
+////            reader.readLine() : 라벨읽음
+//            val reader = BufferedReader(
+//                InputStreamReader(assets.open("labels.txt")))
+//            for (i in 1 until probabilities.capacity()) {
 //                val label: String = reader.readLine()
 //                val probability = probabilities.get(i)
-//                println("$label: $probability")
+//                Log.d("출력", "$label: $probability")
+////                println("$label: $probability")
 //            }
-        } catch (e: IOException) {
-            // File not found?
-        }
+//        } catch (e: IOException) {
+//            // File not found?
+//            Log.d("출력", "File not found?")
+//        }
 
 
 
