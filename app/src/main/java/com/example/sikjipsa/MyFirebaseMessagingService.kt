efefa59daf400fb6b21236e990e10152389c66d6
moon34git <moon34git@gmail.com>
2022-11-23 23:01:45 +0900

    Push notification is added

---
+++ app/src/main/java/com/example/sikjipsa/MyFirebaseMessagingService.kt
@@ -0,0 +1,67 @@
+package com.example.sikjipsa
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.ColorSpace
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val channelID = "notification_channel"
+const val channelName = "com.example.sikjipsa"
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    // generate the notification
+    //attach the notification created with the custom layout
+    //show the notification
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if(remoteMessage.getNotification() != null ){
+            generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
+        }
+    }
+
+    fun getRemoteView(title: String, message: String): RemoteViews{
+        val remoteView = RemoteViews("com.example.sikjipsa", R.layout.notification_layout)
+
+        remoteView.setTextViewText(R.id.title, title)
+        remoteView.setTextViewText(R.id.contents, message)
+        remoteView.setImageViewResource(R.id.app_logo, R.drawable.sikjipsalogo)
+
+        return remoteView
+    }
+
+    fun generateNotification(title: String, message: String){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        // channel ID, name
+
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelID)
+            .setSmallIcon(R.drawable.sikjipsalogo)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title, message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationChannel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        notificationManager.notify(0, builder.build())
+
+    }
+
+
+
+}
