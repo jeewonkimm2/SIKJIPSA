6037ef36dbfa9568ad775074f434cfc97da7d1e1
jeewonkimm2 <jeewoncoding@gmail.com>
2022-11-27 17:08:55 +0900

    ml model working well check

--- app/src/main/java/com/example/sikjipsa/SearchActivity.kt
+++ app/src/main/java/com/example/sikjipsa/SearchActivity.kt
@@ -108,7 +108,107 @@ class SearchActivity : AppCompatActivity() {
 
 
 
+
+
+
+
+        val conditions = CustomModelDownloadConditions.Builder()
+            .requireWifi()  // Also possible: .requireCharging() and .requireDeviceIdle()
+            .build()
+        FirebaseModelDownloader.getInstance()
+            .getModel("model_fp16", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND,
+                conditions)
+            .addOnSuccessListener { model: CustomModel? ->
+                // Download complete. Depending on your app, you could enable the ML
+                // feature, or switch from the local model to the remote model, etc.
+
+                // The CustomModel object contains the local path of the model file,
+                // which you can use to instantiate a TensorFlow Lite interpreter.
+                var interpreter:Interpreter?=null
+                val modelFile = model?.file
+                if (modelFile != null) {
+                    interpreter = Interpreter(modelFile)
+                    Log.d("probabilities","model import done")
+
+                    val plant = getResources().getDrawable(R.drawable.monstera)
+
+                    val bitmap = Bitmap.createScaledBitmap(plant.toBitmap(), 224, 224, true)
+                    val input = ByteBuffer.allocateDirect(224*224*3*4).order(ByteOrder.nativeOrder())
+                    for (y in 0 until 224) {
+                        for (x in 0 until 224) {
+                            val px = bitmap.getPixel(x, y)
+
+                            // Get channel values from the pixel value.
+                            val r = Color.red(px)
+                            val g = Color.green(px)
+                            val b = Color.blue(px)
+
+                            // Normalize channel values to [-1.0, 1.0]. This requirement depends on the model.
+                            // For example, some models might require values to be normalized to the range
+                            // [0.0, 1.0] instead.
+                            val rf = (r - 127) / 255f
+                            val gf = (g - 127) / 255f
+                            val bf = (b - 127) / 255f
+
+                            input.putFloat(rf)
+                            input.putFloat(gf)
+                            input.putFloat(bf)
+                        }
+                    }
+
+                    val bufferSize = 18 * java.lang.Float.SIZE / java.lang.Byte.SIZE
+                    val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
+                    Log.d("probabilities","model is running")
+                    interpreter?.run(input, modelOutput)
+                    Log.d("probabilities","model running done")
+
+
+                    modelOutput.rewind()
+                    val probabilities = modelOutput.asFloatBuffer()
+
+
+                    try {
+                        Log.d("probabilities","ready to show result")
+                        val reader = BufferedReader(
+                            InputStreamReader(assets.open("labels.txt")))
+                        for (i in 0 until probabilities.capacity()) {
+                            val label: String = reader.readLine()
+                            val probability = probabilities.get(i)
+                            Log.d("probabilities","$label: $probability")
+                        }
+                    } catch (e: IOException) {
+                        // File not found?
+                    }
+
+
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
