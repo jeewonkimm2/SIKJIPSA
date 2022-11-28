c7ac5546bd327e7aaffdb134bf01c871947fa0eb
jeewonkimm2 <jeewoncoding@gmail.com>
2022-11-29 01:12:59 +0900

    Searchactivity fixing ongoing

--- app/src/main/java/com/example/sikjipsa/EditProfileActivity.kt
+++ app/src/main/java/com/example/sikjipsa/EditProfileActivity.kt
@@ -3,10 +3,20 @@ package com.example.sikjipsa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sikjipsa.R
+import com.example.sikjipsa.navigation.DetailViewFragment
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        initView()
     }
+
+    private fun initView() {
+        supportFragmentManager.beginTransaction().add(R.id.main_content, DetailViewFragment())
+            .commit()
+
+    }
+
 }
--- app/src/main/java/com/example/sikjipsa/SearchActivity.kt
+++ app/src/main/java/com/example/sikjipsa/SearchActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.app.SearchManager
 import android.content.Intent
 import android.graphics.*
+import android.graphics.Bitmap.createBitmap
 import android.graphics.Bitmap.createScaledBitmap
 import android.net.Uri
 import android.os.Build
@@ -13,6 +14,7 @@ import android.provider.MediaStore
 import android.provider.MediaStore.Images.Media.getBitmap
 import android.util.Log
 import android.view.View.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
 import com.example.sikjipsa.databinding.ActivitySearchBinding
@@ -32,8 +34,9 @@ import java.util.*
 
 class SearchActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
-
-
+    var photoURI: Uri? = null
+    var keywordFromModel:String? = null
+    var probFromModel:kotlin.Float = 0.0f
 
     //    사진부분
     private lateinit var binding: ActivitySearchBinding
@@ -46,6 +49,7 @@ class SearchActivity : AppCompatActivity() {
 
     val PERMISSIONS_REQUEST = 100
 
+
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,112 +58,24 @@ class SearchActivity : AppCompatActivity() {
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var keywordFromModel:String?
-        var probFromModel:kotlin.Float = 0.0f
 
-        binding.imageBtn.setOnClickListener {
 
 
 
+        binding.imageBtn.setOnClickListener {
 
             var photoPickerIntent = Intent(Intent.ACTION_PICK)
             photoPickerIntent.type = "image/*"
             startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
 
-
-            val conditions = CustomModelDownloadConditions.Builder()
-                .requireWifi()
-                .build()
-            FirebaseModelDownloader.getInstance()
-                .getModel("model_fp16", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND,
-                    conditions)
-                .addOnSuccessListener { model: CustomModel? ->
-                    var interpreter:Interpreter?=null
-                    val modelFile = model?.file
-                    if (modelFile != null) {
-                        interpreter = Interpreter(modelFile)
-                        Log.d("probabilities","model import done")
-
-                        val plant = getResources().getDrawable(R.drawable.monstera)
-//                        Log.d("probabilities","$plant")
-
-                        Log.d("probabilities","picture import done")
-
-                        Log.d("probabilities","photoURI $photoURI")
-                        val bitmap = Bitmap.createScaledBitmap(plant.toBitmap(), 224, 224, true)
-
-                        Log.d("probabilities","picture import done")
-                        val input = ByteBuffer.allocateDirect(224*224*3*4).order(ByteOrder.nativeOrder())
-                        for (y in 0 until 224) {
-                            for (x in 0 until 224) {
-                                val px = bitmap.getPixel(x, y)
-
-                                // Get rgb
-                                val r = Color.red(px)
-                                val g = Color.green(px)
-                                val b = Color.blue(px)
-
-//                                Normalizing
-                                val rf = (r - 127) / 255f
-                                val gf = (g - 127) / 255f
-                                val bf = (b - 127) / 255f
-
-                                input.putFloat(rf)
-                                input.putFloat(gf)
-                                input.putFloat(bf)
-                            }
-                        }
-
-                        val bufferSize = 18 * java.lang.Float.SIZE / java.lang.Byte.SIZE
-                        val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
-                        Log.d("probabilities","model is running")
-                        interpreter?.run(input, modelOutput)
-                        Log.d("probabilities","model running done")
-
-
-                        modelOutput.rewind()
-                        val probabilities = modelOutput.asFloatBuffer()
-
-
-                        try {
-                            Log.d("probabilities","ready to show result")
-                            val reader = BufferedReader(
-                                InputStreamReader(assets.open("labels.txt")))
-                            var bestLabel: String? = null
-                            var bestProb: kotlin.Float = 0.0f
-                            for (i in 0 until probabilities.capacity()) {
-                                val label: String = reader.readLine()
-                                val probability = probabilities.get(i)
-                                if(bestProb < probability){
-                                    bestProb = probability
-                                    bestLabel = label
-                                }
-//                                Log.d("probabilities","$label: $probability")
-                            }
-//                            Log.d("probabilities","It is $bestLabel")
-                            probFromModel = bestProb
-                            keywordFromModel = bestLabel
-
-                            val snack = Snackbar.make(it, "$keywordFromModel with $probFromModel%.", Snackbar.LENGTH_SHORT)
-                            snack.show()
-
-                            binding.GOOGLEImg.setOnClickListener {
-                                var intent = Intent(Intent.ACTION_WEB_SEARCH)
-                                intent.putExtra(SearchManager.QUERY, keywordFromModel)
-                                startActivity(intent)
-                            }
-                        } catch (e: IOException) {
-                            // File not found?
-                        }
-
-
-                    }
-
-                }
             binding.imageBtn.visibility = INVISIBLE
             binding.GOOGLEImg.visibility = VISIBLE
             binding.SIKJIPSAImg.visibility = VISIBLE
         }
+
+
+
+
 
         binding.keywordBtn.setOnClickListener {
             binding.keywordBtn.visibility = GONE
@@ -187,14 +103,14 @@ class SearchActivity : AppCompatActivity() {
 
     }
 
-    var photoURI: Uri? = null
-    var bitmapFinal:Bitmap? = null
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     super.onActivityResult(requestCode, resultCode, data)
     if (requestCode == PICK_IMAGE_FROM_ALBUM) {
         if (resultCode == AppCompatActivity.RESULT_OK) {
             photoURI = data?.data
+            var bitmapFinal:Bitmap?=null
 
             try{
                 photoURI?.let{
@@ -219,6 +135,109 @@ class SearchActivity : AppCompatActivity() {
 
                 }
 
+
+
+
+
+                val conditions = CustomModelDownloadConditions.Builder()
+                    .requireWifi()
+                    .build()
+                FirebaseModelDownloader.getInstance()
+                    .getModel("model_fp16", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND,
+                        conditions)
+                    .addOnSuccessListener { model: CustomModel? ->
+                        var interpreter: Interpreter? = null
+                        val modelFile = model?.file
+
+                        if (modelFile != null) {
+                            interpreter = Interpreter(modelFile)
+                            Log.d("probabilities", "model import done")
+
+                            val plant = getResources().getDrawable(R.drawable.monstera)
+                            Log.d("probabilities","$plant")
+
+                            Log.d("probabilities","picture import done")
+
+                            Log.d("probabilities","${photoURI}")
+                            Log.d("probabilities","${bitmapFinal}")
+
+
+                            val bitmap = Bitmap.createScaledBitmap(plant.toBitmap(), 224, 224, true)
+
+                            Log.d("probabilities","picture import done")
+                            val input = ByteBuffer.allocateDirect(224*224*3*4).order(ByteOrder.nativeOrder())
+                            for (y in 0 until 224) {
+                                for (x in 0 until 224) {
+                                    val px = bitmap.getPixel(x, y)
+
+                                    // Get rgb
+                                    val r = Color.red(px)
+                                    val g = Color.green(px)
+                                    val b = Color.blue(px)
+
+//                                Normalizing
+                                    val rf = (r - 127) / 255f
+                                    val gf = (g - 127) / 255f
+                                    val bf = (b - 127) / 255f
+
+                                    input.putFloat(rf)
+                                    input.putFloat(gf)
+                                    input.putFloat(bf)
+                                }
+                            }
+
+                            val bufferSize = 18 * java.lang.Float.SIZE / java.lang.Byte.SIZE
+                            val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
+                            Log.d("probabilities","model is running")
+                            interpreter?.run(input, modelOutput)
+                            Log.d("probabilities","model running done")
+
+
+                            modelOutput.rewind()
+                            val probabilities = modelOutput.asFloatBuffer()
+
+
+                            try {
+                                Log.d("probabilities","ready to show result")
+                                val reader = BufferedReader(
+                                    InputStreamReader(assets.open("labels.txt")))
+                                var bestLabel: String? = null
+                                var bestProb: kotlin.Float = 0.0f
+                                for (i in 0 until probabilities.capacity()) {
+                                    val label: String = reader.readLine()
+                                    val probability = probabilities.get(i)
+                                    if(bestProb < probability){
+                                        bestProb = probability
+                                        bestLabel = label
+                                    }
+//                                Log.d("probabilities","$label: $probability")
+                                }
+//                            Log.d("probabilities","It is $bestLabel")
+                                probFromModel = bestProb
+                                keywordFromModel = bestLabel
+
+//                        val snack = Snackbar.make(it, "$keywordFromModel with $probFromModel%.", Snackbar.LENGTH_SHORT)
+//                        snack.show()
+
+                                binding.GOOGLEImg.setOnClickListener {
+                                    var intent = Intent(Intent.ACTION_WEB_SEARCH)
+                                    intent.putExtra(SearchManager.QUERY, keywordFromModel)
+                                    startActivity(intent)
+                                }
+                            } catch (e: IOException) {
+                                // File not found?
+                            }
+
+
+                        }
+                    }
+
+
+
+
+
+
+
             }catch(e:Exception){
                 e.printStackTrace()
             }
@@ -227,5 +246,10 @@ class SearchActivity : AppCompatActivity() {
         }
     }
     }
+
+
+
+
+
 }
 
